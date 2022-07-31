82ad0c7644851c55f9087830fd8ebdfc005656db
Ashish Nimrot <ashishnimrot@gmail.com>
2022-07-31 19:42:03 +0530

    Setup app

---
+++ src/app/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Button from '@mui/material/Button';
+
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Button variant="outlined">Test</Button>
+      </header>
+    </div>
+  );
+}
+
+export default App;
